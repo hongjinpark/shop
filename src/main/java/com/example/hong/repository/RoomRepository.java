56450ba368d4a7287a6f816286c64fe4b9ddd58f
AnGwangMin1 <dksrhkd22@gmail.com>
2023-03-17 18:25:35 +0900

    랄라라라랄라

--- src/main/java/com/example/hong/repository/RoomRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.hong.repository;
-
-import com.example.hong.entity.Room;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RoomRepository extends JpaRepository<Room, Integer> {
-
-
-}
--- src/main/java/com/example/hong/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.hong.repository;
-
-import com.example.hong.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, String> {
-
-}
