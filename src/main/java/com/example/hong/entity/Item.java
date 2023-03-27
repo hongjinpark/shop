5eee162d1173bec4bee6b0047b49ba55c9b0b1c7
hyeok-min <gurals04@gmail.com>
2023-03-27 17:43:00 +0900

    item entity 변경

--- src/main/java/com/example/hong/entity/Item.java
+++ src/main/java/com/example/hong/entity/Item.java
@@ -1,32 +1,46 @@
 package com.example.hong.entity;
 
+
 import com.example.hong.enums.ItemSellStatus;
+
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
+
 import java.util.ArrayList;
 import java.util.List;
 
 @Getter
-@Entity
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "item")
 public class Item {
 
     @Id
-    @GeneratedValue
     @Column(name = "item_id")
-    private Long id;
-    private String itemName;
-    private int price;
-    private int stockNumber;
-    private String itemDetail;
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id; //상품 코드
+
+    @Column(nullable = false, length = 50)
+    private String itemName; //상품명
+
+    @Column(name = "price", nullable = false)
+    private int price; //가격
+
+    @Column(nullable = false)
+    private int stockNumber; //재고수량
+
+    @Lob
+    @Column(nullable = false)
+    private String itemDetail; //상품 상세 설명
 
     @Enumerated(EnumType.STRING)
-    private ItemSellStatus itemSellStatus;
+    private ItemSellStatus itemSellStatus; //상품 판매 상태
 
     @OneToMany(mappedBy = "item")
     private List<OrderItem> orderItem=new ArrayList<>();
@@ -40,5 +54,4 @@ public class Item {
         this.itemDetail = itemDetail;
         this.itemSellStatus=itemSellStatus;
     }
-
 }
