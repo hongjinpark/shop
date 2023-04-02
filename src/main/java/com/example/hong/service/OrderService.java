19d1818561ec8158eba1e9568ac950edc003def1
ParkHongJin <ektkfk180@naver.com>
2023-04-02 18:56:34 +0900

    Order 엔티티 추가

---
+++ src/main/java/com/example/hong/service/OrderService.java
@@ -0,0 +1,11 @@
+package com.example.hong.service;
+
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+
+@RequiredArgsConstructor
+@Service
+public class OrderService {
+}
