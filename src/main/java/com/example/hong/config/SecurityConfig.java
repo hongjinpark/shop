d4bf80d18774e6e6c9a3442723598c42d22614e0
hyeok-min <gurals04@gmail.com>
2023-04-07 13:47:20 +0900

    수정

--- src/main/java/com/example/hong/config/SecurityConfig.java
+++ src/main/java/com/example/hong/config/SecurityConfig.java
@@ -1,55 +1,28 @@
 package com.example.hong.config;
 
-import lombok.RequiredArgsConstructor;
+
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
-@Configuration
-@RequiredArgsConstructor
-@EnableWebSecurity
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Configuration
+    public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-    private final LoginSecurityService loginSecurityService;
+        @Bean
+        public PasswordEncoder passwordEncoder(){
+            return new BCryptPasswordEncoder();
+        }
 
+        @Override
+        protected void configure(HttpSecurity http) throws Exception {
+            http.cors().disable()
+                    .csrf().disable()
+                    .formLogin().disable()
+                    .headers().frameOptions().disable();
+        }
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable()
-
-                .authorizeRequests()
-                .antMatchers("/init/login", "/member/new").permitAll()  //로그인없이 접근가능
-                .anyRequest().authenticated()
-                .and()
-                .formLogin()//'formLogin()'에서 폼방식 로그인을 사용할 것임을 알림
-//                .loginPage("/")//커스텀 페이지로 로그인 페이지변경
-                .loginProcessingUrl("/member/loginPro")//form 태그의 Action URL.
-                .usernameParameter("userEmail")
-                .passwordParameter("passWord")
-                .defaultSuccessUrl("/home")//로그인인증 성공후 갈 페이지
-                .failureForwardUrl("/loginFail")
-                .permitAll()
-                .and()
-                .logout().logoutUrl("/member/logout").logoutSuccessUrl("/");
-
-    }
-    @Override
-    public void configure(WebSecurity web) throws Exception {   //화면 깨지지않게 하기
-        web.ignoring().antMatchers("/css/**");
-        web.ignoring().antMatchers("/js/**");
-        web.ignoring().antMatchers("/images/**");
-        web.ignoring().antMatchers("/files/**");
-    }
-
-    @Bean
-    public BCryptPasswordEncoder getpasswordEncoder() { return new BCryptPasswordEncoder(); }
-
-    public void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(loginSecurityService).passwordEncoder(getpasswordEncoder());
-    }
 }
