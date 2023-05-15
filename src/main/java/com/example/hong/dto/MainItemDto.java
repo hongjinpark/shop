5e2c16de26622fa472f188fef547719aed0486cd
ektkfk180@naver.com <ektkfk180@naver.com>
2023-05-15 17:24:12 +0900

    id > item_id 수정

--- src/main/java/com/example/hong/dto/MainItemDto.java
+++ src/main/java/com/example/hong/dto/MainItemDto.java
@@ -7,15 +7,15 @@ import lombok.Setter;
 @Getter @Setter
 public class MainItemDto {
 
-    private Long id;
+    private Long item_id;
     private String itemName;
     private String itemDetail;
     private String imgUrl;
     private Integer price;
 
     @QueryProjection
-    public MainItemDto(Long id, String itemName, String itemDetail, String imgUrl, Integer price) {
-        this.id = id;
+    public MainItemDto(Long item_id, String itemName, String itemDetail, String imgUrl, Integer price) {
+        this.item_id = item_id;
         this.itemName = itemName;
         this.itemDetail = itemDetail;
         this.imgUrl = imgUrl;
